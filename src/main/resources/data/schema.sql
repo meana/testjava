4983854e30f8ff3813785f5b6286544e1fce8e39
Ramón Meana <ramon.meana@kroger.com>
2024-01-25 19:14:01 +0100

    Database config

---
+++ src/main/resources/data/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE prices (
+    brand_id INT,
+    start_date TIMESTAMP,
+    end_date TIMESTAMP,
+    price_list INT,
+    product_id INT,
+    priority INT,
+    price DECIMAL(10, 2),
+    curr VARCHAR(3)
+);
