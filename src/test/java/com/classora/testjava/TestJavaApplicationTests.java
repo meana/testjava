ca9db1c9a594d8cb6aca6290f3b3ad6db9f87d83
Ramón Meana <ramon.meana@kroger.com>
2024-01-25 18:40:33 +0100

    Initial Commit

---
+++ src/test/java/com/classora/testjava/TestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classora.testjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
