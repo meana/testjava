0484db7937d3f730e995b93f4695fab9199f110c
Ramón Meana <ramon.meana@kroger.com>
2024-01-30 20:23:54 +0100

    Database entity

---
+++ src/main/java/com/classora/testjava/model/Prices.java
@@ -0,0 +1,44 @@
+package com.classora.testjava.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "prices")
+public class Prices {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "brand_id")
+    private Integer brandId;
+
+    @Column(name = "start_date")
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date")
+    private LocalDateTime endDate;
+
+    @Column(name = "price_list")
+    private Integer priceList;
+
+    @Column(name = "product_id")
+    private Integer productId;
+
+    private Integer priority;
+
+    private BigDecimal price;
+
+    private String curr;
+
+}
